e44404a8690f2b659d2cb79df6c434cc5abaf86e
Maycon Marigo <maycon.marigo@hotmail.com>
2022-10-03 22:20:16 -0300

    Criar roteamento e telas iniciais do tabs navigator

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,35 +1,55 @@
 import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import Training from "./Tabs/training/Training";
+import Agenda from "./Tabs/agenda/Agenda";
+import FindPersonal from "./Tabs/find-personal/FindPersonal";
+import Account from "./Tabs/account/Account";
 
 export default function Home() {
   const Tab = createBottomTabNavigator();
 
-  const Home = () => (
-    <View>
-      <Text>Home</Text>
-    </View>
-  );
-
-  const Home2 = () => (
-    <View>
-      <Text>Home</Text>
-    </View>
-  );
-
   return (
     <NavigationContainer independent>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarStyle: {
+            backgroundColor: "#2E2E2E",
+          },
+          tabBarActiveTintColor: "#FFD25A",
+          tabBarInactiveTintColor: "#fff",
+          tabBarIcon: ({ focused }) => {
+            let tabs: any = {
+              Treinos: "dumbbell",
+              Agenda: "calendar-month",
+              Personal: "arm-flex",
+              Account: "account",
+            };
+            return (
+              <Icon
+                name={tabs[route.name]}
+                color={focused ? "#FFD25A" : "#fff"}
+                size={26}
+              />
+            );
+          },
+        })}
+      >
         <Tab.Screen
-          options={{ headerShown: false }}
-          name="Home"
-          component={Home}
+          options={() => ({
+            tabBarIconStyle: {
+              marginBottom: -4,
+              marginRight: 4,
+            },
+          })}
+          name="Treinos"
+          component={Training}
         />
-        <Tab.Screen
-          options={{ headerShown: false }}
-          name="Home2"
-          component={Home2}
-        />
+        <Tab.Screen name="Agenda" component={Agenda} />
+        <Tab.Screen name="Personal" component={FindPersonal} />
+        <Tab.Screen name="Account" component={Account} />
       </Tab.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/home/Tabs/agenda/Agenda.tsx
@@ -0,0 +1,8 @@
+import { View, Text } from "react-native";
+export default function Agenda() {
+  return (
+    <View>
+      <Text>Agenda</Text>
+    </View>
+  );
+}
