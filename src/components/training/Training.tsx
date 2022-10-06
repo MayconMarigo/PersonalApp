2f8e9f4498cf0ce3e653c31797ee5b1bd044f914
Maycon Marigo <maycon.marigo@hotmail.com>
2022-10-06 12:27:10 -0300

    Adição tela home

---
+++ src/components/training/Training.tsx
@@ -0,0 +1,81 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
+import Icon from "react-native-vector-icons/AntDesign";
+import { TrainingCardProps } from "./interface";
+
+export default function TrainingCard(props: TrainingCardProps) {
+  return (
+    <TouchableOpacity
+      style={{
+        flex: 1,
+        height: 250,
+        backgroundColor: "#FFE15A",
+        borderRadius: 10,
+        marginBottom: 15,
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          borderTopLeftRadius: 10,
+          borderTopRightRadius: 10,
+          justifyContent: "center",
+          backgroundColor: "#dedede",
+          position: "relative",
+        }}
+      >
+        <View
+          style={{
+            borderRadius: 999,
+            backgroundColor: "#2E2E2E",
+            marginLeft: 20,
+            height: 110,
+            width: 110,
+          }}
+        />
+        <LinearGradient
+          style={{
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+          }}
+          locations={[0.55, 1]}
+          colors={["transparent", "rgba(0,0,0,0.7)"]}
+        />
+      </View>
+      <View
+        style={{ height: "40%", padding: 20, justifyContent: "space-between" }}
+      >
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Text style={{ fontSize: 10 }}>PERSONAL • </Text>
+          <Text>{props.personal}</Text>
+        </View>
+        <View
+          style={{
+            width: "100%",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ fontSize: 18 }}>{props.training}</Text>
+          <View
+            style={{
+              flexDirection: "row",
+              width: 70,
+              backgroundColor: "#fff",
+              justifyContent: "space-around",
+              padding: 5,
+              paddingRight: 8,
+              borderRadius: 16,
+            }}
+          >
+            <Icon name="star" size={20} color="#FFE15A" />
+            <Text>{props.value}</Text>
+          </View>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+}
--- src/screens/home/Tabs/training/Training.tsx
+++ src/screens/home/Tabs/training/Training.tsx
@@ -1,8 +1,126 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { useState } from "react";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { Divider } from "@react-native-material/core";
+import TrainingCard from "../../../../components/training/Training";
+
 export default function Training() {
+  const styles = StyleSheet.create({
+    container_gray: {
+      flex: 1,
+      backgroundColor: "#F2F2F2",
+      paddingHorizontal: 15,
+      paddingTop: 15,
+    },
+  });
+
+  const [search, setSearch] = useState<string>("");
+
   return (
-    <View>
-      <Text>Training</Text>
+    <View style={styles.container_gray}>
+      <View
+        style={{
+          width: "100%",
+          maxHeight: 40,
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <View
+          style={{
+            width: "90%",
+            height: 40,
+            paddingHorizontal: 10,
+            flexDirection: "row",
+            borderRadius: 8,
+            alignItems: "center",
+            backgroundColor: "#B6B6B6",
+          }}
+        >
+          <Icon name="dumbbell" size={18} />
+          <TextInput
+            style={{ marginLeft: 15 }}
+            placeholder="Busque seus treinos"
+          />
+        </View>
+        <View
+          style={{
+            flex: 1,
+            height: "100%",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <TouchableOpacity>
+            <Icon name="filter-menu" size={18} />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <View style={{ width: "100%", marginTop: 25 }}>
+        <View
+          style={{ width: "100%", flexDirection: "row", alignItems: "center" }}
+        >
+          <Text
+            style={{ fontWeight: "900", paddingRight: 10, color: "#2E2E2E" }}
+          >
+            MEUS TREINOS
+          </Text>
+          <Divider
+            style={{ width: "100%", height: 2, backgroundColor: "#2E2E2E" }}
+          />
+        </View>
+      </View>
+
+      <View style={{ width: "100%", marginVertical: 24 }}>
+        <Text style={{ textAlign: "center" }}>
+          Ops ! Parece que você ainda não possui treinos registrados :(
+        </Text>
+        <View style={{ flexDirection: "row", justifyContent: "center" }}>
+          <Text>Você pode </Text>
+          <TouchableOpacity
+            style={{
+              alignItems: "center",
+            }}
+          >
+            <Text
+              style={{ fontWeight: "900", textDecorationLine: "underline" }}
+            >
+              procurar por profissionais aqui !
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <ScrollView>
+        <TrainingCard
+          personal={"Maycon Marigo"}
+          training="Aeróbico"
+          value={"4.2"}
+        />
+        <TrainingCard
+          personal={"Maycon Marigo"}
+          training="Aeróbico"
+          value={"4.2"}
+        />
+        <TrainingCard
+          personal={"Maycon Marigo"}
+          training="Aeróbico"
+          value={"4.2"}
+        />
+        <TrainingCard
+          personal={"Maycon Marigo"}
+          training="Aeróbico"
+          value={"4.2"}
+        />
+      </ScrollView>
     </View>
   );
 }
